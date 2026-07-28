f603856c70161d2ac361f27da78207ba31286bf9
Maryam mossad <mariammosaad033@gmail.com>
2026-07-28 12:12:26 +0300

    Create Problem 3.cpp

---
+++ Problem 3.cpp
@@ -0,0 +1,39 @@
+//Write a program to ask the user to enter a number, then Print "Odd" if its odd, Or "Even" if its even.
+#include <iostream>
+using namespace std;
+enum enNumberType{odd=1,even=2};
+int ReadNumber()
+{
+	int number;
+	cout << "Please enter a number : ";
+	cin >> number;
+	return number;
+}
+enNumberType CheckNumberType(int number)
+{
+	int result = number % 2;
+	if (result == 0)
+	{
+		return enNumberType::even;
+	}
+	else
+	{
+		return enNumberType::odd;
+	}
+}
+void PrintNumberType(enNumberType number)
+{
+	if (number == enNumberType::even)
+	{
+		cout << "The number is even"<<endl;
+	}
+	else
+	{
+		cout << "The number is odd"<<endl;
+	}
+}
+int main()
+{
+	PrintNumberType(CheckNumberType(ReadNumber()));
+	return 0;
+}
